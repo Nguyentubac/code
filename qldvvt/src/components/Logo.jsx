45aa6de6a7e926063db9d800d7e3be246abc9ed3
Nguyentubac <ntbac.dhti15a14hn@sv.uneti.edu.vn>
2025-03-02 00:05:25 +0700

    req

--- qldvvt/src/components/Logo.jsx
+++ qldvvt/src/components/Logo.jsx
@@ -1,9 +1,15 @@
+// src/components/Logo.jsx
 import React from 'react';
-//import styles from './Logo.module.css';
-import styles from '../public/logo.module.css';
+import styles from './Logo.module.css';
+import logo from '../assets/anh1.png';
 function Logo() {
   return (
     <div className={styles.logoContainer}>
+      <img 
+        src={logo} 
+        alt="Logo" 
+        className={styles.logoImage} 
+      />
     </div>
   );
 }
